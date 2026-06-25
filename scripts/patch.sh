35a1da3d115a517a0de5d62d12576b6c2ad022ef
Kafva <jonas@kafva.one>
2026-06-25 07:30:42 +0200

    Add install script

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-AOSP=$1
-
-if [ ! -d "$1" ]; then
-    echo "$0: <aosp>"
-    exit 1
-fi
-
 while read -r f; do
-    (cd $AOSP/$(dirname ${f##./patches/}) && git apply -v -C100 $f)
+    (cd $1/$(dirname ${f##./patches/}) && git apply -v -C100 $f)
 done < <(find ./patches -type f -name '*.diff')
-
-
